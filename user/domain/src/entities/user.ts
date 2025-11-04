62a530297fd47702e49bd59bb89915a50d3a424b
Thiago <thiagodelgado.dev@gmail.com>
2025-11-04 11:46:48 -0300

    feat: create User interface extending Entity, Person, and TimestampedEntity

---
+++ user/domain/src/entities/user.ts
@@ -0,0 +1,8 @@
+import { Entity, Person, TimestampedEntity } from "domain-lib";
+
+export interface User extends Entity, Person, TimestampedEntity {
+  userName?: string | null;
+  emailVerified: boolean;
+  hashedPassword: string;
+  enabled: boolean;
+}
