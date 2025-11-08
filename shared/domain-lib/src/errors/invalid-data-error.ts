f4546c72130f0a0d5514da5f78e951de1ed554ae
Thiago <thiagodelgado.dev@gmail.com>
2025-11-08 19:52:02 -0300

    feat: add InvalidDataError class and update index to export it

---
+++ shared/domain-lib/src/errors/invalid-data-error.ts
@@ -0,0 +1,7 @@
+import { BaseError } from "./base-error";
+
+export class InvalidDataError extends BaseError<"INVALID_DATA_ERROR"> {
+  constructor(errors?: Record<string, string>) {
+    super("INVALID_DATA_ERROR", 400, errors);
+  }
+}
