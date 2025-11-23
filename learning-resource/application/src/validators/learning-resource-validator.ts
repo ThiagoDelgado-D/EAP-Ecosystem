695b78c43d15576ca74aad78d1c54f566ab7dbbc
Thiago <thiagodelgado.dev@gmail.com>
2025-11-23 10:55:16 -0300

    refactor: remove isValidUpdatePayload method from LearningResourceValidator interface

--- learning-resource/application/src/validators/learning-resource-validator.ts
+++ learning-resource/application/src/validators/learning-resource-validator.ts
@@ -1,8 +1,6 @@
 import { AddResourceRequestModel } from "../use-cases/learning-resource/add-resource";
-import { UpdateResourceRequestModel } from "../use-cases/learning-resource/update-resource";
 
 export interface LearningResourceValidator {
   isValidAddPayload(payload: AddResourceRequestModel): Promise<boolean>;
-  isValidUpdatePayload(payload: UpdateResourceRequestModel): Promise<boolean>;
   isValidUrl(url?: string): Promise<boolean>;
 }
