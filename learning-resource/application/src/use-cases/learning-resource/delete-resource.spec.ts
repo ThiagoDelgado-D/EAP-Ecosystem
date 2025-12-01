de9c12e9bc62183d813a6585609a868981a0d72f
Thiago <thiagodelgado.dev@gmail.com>
2025-12-01 11:05:27 -0300

    feat: implement deleteResource functionality with tests for valid and invalid IDs

---
+++ learning-resource/application/src/use-cases/learning-resource/delete-resource.spec.ts
@@ -0,0 +1,61 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { mockLearningResourceRepository } from "../../mocks/mock-learning-resource-repository";
+import { mockCryptoService, UUID } from "domain-lib";
+import {
+  DifficultyType,
+  EnergyLevelType,
+  ResourceStatusType,
+} from "@learning-resource/domain";
+import { deleteResource } from "./delete-resource";
+import { LearningResourceNotFoundError } from "../../errors/learning-resource-not-found";
+
+describe("deleteResource", () => {
+  let cryptoService: ReturnType<typeof mockCryptoService>;
+  let learningResourceRepository: ReturnType<
+    typeof mockLearningResourceRepository
+  >;
+  let resourceTypeId: UUID;
+
+  beforeEach(async () => {
+    cryptoService = mockCryptoService();
+    learningResourceRepository = mockLearningResourceRepository([]);
+    resourceTypeId = await cryptoService.generateUUID();
+  });
+
+  test("With valid id, should delete the resource", async () => {
+    const resourceId = await cryptoService.generateUUID();
+
+    await learningResourceRepository.save({
+      id: resourceId,
+      title: "Typescript Advanced",
+      url: "https://www.typescriptlang.org/docs/handbook/advanced-types.html",
+      difficulty: DifficultyType.HIGH,
+      energyLevel: EnergyLevelType.LOW,
+      status: ResourceStatusType.PENDING,
+      topicIds: [],
+      typeId: resourceTypeId,
+      estimatedDuration: {
+        isEstimated: true,
+        value: 120,
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await deleteResource({ learningResourceRepository }, { id: resourceId });
+
+    const foundResource = await learningResourceRepository.findById(resourceId);
+
+    expect(foundResource).toBeNull();
+  });
+  test("With invalid id, should return LearningResourceNotFoundError", async () => {
+    const resourceId = await cryptoService.generateUUID();
+
+    const result = await deleteResource(
+      { learningResourceRepository },
+      { id: resourceId }
+    );
+
+    expect(result).toBeInstanceOf(LearningResourceNotFoundError);
+  });
+});
