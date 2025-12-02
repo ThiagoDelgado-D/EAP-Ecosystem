71b10646af3b3c9aaec68c81b378f36dae2ede32
Thiago <thiagodelgado.dev@gmail.com>
2025-12-02 20:40:58 -0300

    chore: add missing export in index file

--- learning-resource/application/src/use-cases/learning-resource/index.ts
+++ learning-resource/application/src/use-cases/learning-resource/index.ts
@@ -2,3 +2,4 @@ export * from "./add-resource";
 export * from "./delete-resource";
 export * from "./list-resource";
 export * from "./get-resource-by-id";
+export * from "./get-resources-by-filter";
