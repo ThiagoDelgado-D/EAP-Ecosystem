633987d609584ef131c496d93ed665a9915a092e
Thiago <thiagodelgado.dev@gmail.com>
2025-12-02 22:01:56 -0300

    feat: add index file to export mock repositories and validators

---
+++ learning-resource/application/src/mocks/index.ts
@@ -0,0 +1,4 @@
+export * from "./validators/mock-learning-resource-validator";
+export * from "./mock-learning-resource-repository";
+export * from "./mock-resource-type-repository";
+export * from "./mock-topic-repository";
