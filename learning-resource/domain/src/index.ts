f0dc6de5dcaacfa846eede99f4361e8192547caf
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-01-14 16:23:15 -0300

    refactor: update import paths to include .js extensions for entity and repository files

--- learning-resource/domain/src/index.ts
+++ learning-resource/domain/src/index.ts
@@ -1,11 +1,11 @@
 // ============= ENTITIES =============
 
-export * from "./entities/learning-resource";
-export * from "./entities/resource-type";
-export * from "./entities/topic";
+export * from "./entities/learning-resource.js";
+export * from "./entities/resource-type.js";
+export * from "./entities/topic.js";
 
 // ============= REPOSITORIES =============
 
-export * from "./repositories/ITopic-repository";
-export * from "./repositories/lLearning-resource-repository";
-export * from "./repositories/IResource-type-repository";
+export * from "./repositories/ITopic-repository.js";
+export * from "./repositories/lLearning-resource-repository.js";
+export * from "./repositories/IResource-type-repository.js";
