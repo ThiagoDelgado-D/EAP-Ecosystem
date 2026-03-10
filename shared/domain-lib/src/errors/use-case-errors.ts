d9d01fd54951cc83325713b95ffcf4fd9397be6c
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-03-10 17:53:22 -0300

    fix(domain-lib): extract UnwrapReturn helper to ensure distributive evaluation over mixed sync/async use case maps

--- shared/domain-lib/src/errors/use-case-errors.ts
+++ shared/domain-lib/src/errors/use-case-errors.ts
@@ -1,9 +1,8 @@
 import type { BaseError } from "./base-error.js";
 
+type UnwrapReturn<R> = R extends Promise<infer U> ? U : R;
+
 export type UseCaseErrors<T extends Record<string, (...args: any[]) => any>> =
   T[keyof T] extends (...args: any[]) => infer R
-    ? Extract<
-        Exclude<R extends Promise<infer U> ? U : R, void | undefined>,
-        BaseError
-      >
+    ? Extract<Exclude<UnwrapReturn<R>, void | undefined>, BaseError>
     : never;
