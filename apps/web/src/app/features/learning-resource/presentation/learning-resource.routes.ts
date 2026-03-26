f15b89da056e15598b7b9e3442e8f283272c8ba2
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-03-25 21:01:36 -0300

    feat(web): add /add route for AddResourceComponent

--- apps/web/src/app/features/learning-resource/presentation/learning-resource.routes.ts
+++ apps/web/src/app/features/learning-resource/presentation/learning-resource.routes.ts
@@ -5,4 +5,9 @@ export const learningResourceRoutes: Routes = [
     path: '',
     loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('./add-resource/add-resource.component').then((m) => m.AddResourceComponent),
+  },
 ];
