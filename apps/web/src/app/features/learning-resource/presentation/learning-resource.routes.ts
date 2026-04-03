4fcf9b626044d8fb0a6e0f78b8666471179cd523
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-04-03 10:30:55 -0300

    feat(routing): wrap add resource routes with shell layout

--- apps/web/src/app/features/learning-resource/presentation/learning-resource.routes.ts
+++ apps/web/src/app/features/learning-resource/presentation/learning-resource.routes.ts
@@ -20,31 +20,39 @@ export const learningResourceRoutes: Routes = [
         path: 'resources',
         loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
       },
+      {
+        path: 'add/guided',
+        loadComponent: () =>
+          import('./add-resource/guided/guided-form.component').then((m) => m.GuidedFormComponent),
+      },
+      {
+        path: 'add/url',
+        loadComponent: () =>
+          import('./add-resource/url-import/url-import.component').then(
+            (m) => m.UrlImportComponent,
+          ),
+      },
+      {
+        path: 'add/voice',
+        loadComponent: () =>
+          import('./add-resource/voice/voice-capture.component').then(
+            (m) => m.VoiceCaptureComponent,
+          ),
+      },
+      {
+        path: 'add/import',
+        loadComponent: () =>
+          import('./add-resource/file-import/file-import.component').then(
+            (m) => m.FileImportComponent,
+          ),
+      },
+      {
+        path: 'add',
+        loadComponent: () =>
+          import('./add-resource/add-resource-hub.component').then(
+            (m) => m.AddResourceHubComponent,
+          ),
+      },
     ],
-  },
-  {
-    path: 'add',
-    loadComponent: () =>
-      import('./add-resource/add-resource-hub.component').then((m) => m.AddResourceHubComponent),
-  },
-  {
-    path: 'add/guided',
-    loadComponent: () =>
-      import('./add-resource/guided/guided-form.component').then((m) => m.GuidedFormComponent),
-  },
-  {
-    path: 'add/url',
-    loadComponent: () =>
-      import('./add-resource/url-import/url-import.component').then((m) => m.UrlImportComponent),
-  },
-  {
-    path: 'add/voice',
-    loadComponent: () =>
-      import('./add-resource/voice/voice-capture.component').then((m) => m.VoiceCaptureComponent),
-  },
-  {
-    path: 'add/import',
-    loadComponent: () =>
-      import('./add-resource/file-import/file-import.component').then((m) => m.FileImportComponent),
   },
 ];
