0fa6a8034c8df347ebcd7b82bae629e7c5b31e2f
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-05-01 00:26:48 -0300

    chore: add export to complete onboarding use case

--- user/application/src/use-case-map.ts
+++ user/application/src/use-case-map.ts
@@ -1,9 +1,14 @@
 import type { UseCaseErrors } from "domain-lib";
-import { requestSignIn, verifySignIn } from "./use-cases/index.js";
+import {
+  requestSignIn,
+  verifySignIn,
+  completeOnboarding,
+} from "./use-cases/index.js";
 
 export const userUseCaseMap = {
   requestSignIn,
   verifySignIn,
+  completeOnboarding,
 } as const;
 
 export type UserUseCaseMap = typeof userUseCaseMap;
--- user/application/src/use-cases/auth/index.ts
+++ user/application/src/use-cases/auth/index.ts
@@ -1,2 +1,3 @@
 export * from "./request-sign-in.js";
 export * from "./verify-sign-in.js";
+export * from "./complete-onboarding.js";
