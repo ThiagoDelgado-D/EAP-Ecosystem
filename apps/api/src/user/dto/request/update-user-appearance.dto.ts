79de61a9dc3fa33775854b2eed7503a07d3e9d01
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-06-17 17:03:56 -0300

    fix(api): reject null appearance fields and enforce timezone max length in UpdateUserAppearanceDto

--- apps/api/src/user/dto/request/update-user-appearance.dto.ts
+++ apps/api/src/user/dto/request/update-user-appearance.dto.ts
@@ -1,24 +1,30 @@
-import { IsBoolean, IsIn, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsIn, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
 import { LanguageCode, StartOfWeek } from "@user/domain";
 
 export class UpdateUserAppearanceDto {
   @IsOptional()
+  @IsNotEmpty()
   @IsIn(Object.values(LanguageCode))
   language?: string;
 
   @IsOptional()
+  @IsNotEmpty()
   @IsString()
+  @MaxLength(50)
   timezone?: string;
 
   @IsOptional()
+  @IsNotEmpty()
   @IsIn(Object.values(StartOfWeek))
   startOfWeek?: string;
 
   @IsOptional()
+  @IsNotEmpty()
   @IsBoolean()
   reduceMotion?: boolean;
 
   @IsOptional()
+  @IsNotEmpty()
   @IsBoolean()
   compactMode?: boolean;
 }
