8b9e4c1e4a38eba1b65b3c14b1a8255b2b843b54
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-09-19 21:10:16 -0300

    feat: export ExtendSessionDto in request index

--- apps/api/src/pomodoro/dto/request/index.ts
+++ apps/api/src/pomodoro/dto/request/index.ts
@@ -2,4 +2,5 @@ export * from "./session-target.dto.js";
 export * from "./start-session.dto.js";
 export * from "./switch-target.dto.js";
 export * from "./extend-break.dto.js";
+export * from "./extend-session.dto.js";
 export * from "./get-history.dto.js";
