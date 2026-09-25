95248750803e54ad885ad48dbbb8cf64d65e4070
Thiago Delgado <thiagodelgado.dev@gmail.com>
2026-09-25 15:47:57 -0300

    feat: add SegmentTargetForbiddenError to handle forbidden segment target scenarios

--- pomodoro/application/src/errors/index.ts
+++ pomodoro/application/src/errors/index.ts
@@ -1,5 +1,6 @@
 export * from "./session-already-active.js";
 export * from "./ambiguous-path-target.js";
+export * from "./segment-target-forbidden.js";
 export * from "./session-not-found.js";
 export * from "./session-forbidden.js";
 export * from "./session-not-active.js";
